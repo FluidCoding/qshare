da2799b992ab39dd76858569fd334e38ed9b4c00
FluidCoding <sonnie130@gmail.com>
2015-07-24 05:33:11 -0400

    Refactoring Queue Object

--- queue.js
+++ queue.js
@@ -1,1 +1,58 @@
-queue
+var dataRef = new Firebase('https://youtubeq.firebaseio.com/queueList');
+var logging = 1;
+var queue;
+var QueueList = {
+	refId : [], 
+	currentIndex : 0, 
+	titleText : [],
+	thumbnail : [],
+	printQueue: function(){
+		for(var i = 0; i<this.refId.length; i++)
+			document.write(i + ") " + this.refId[i] + " : " + this.titleText[i] + " : " + "<img src=\"" + 
+			this.thumbnail[i] + "\" />");
+	},
+	syncQueue: function(){
+	},
+	getPushable: function()
+	{
+		return({
+				index: this.currentIndex,
+				id: this.refId,
+				title: this.titleText,
+				img: this.thumbnail});
+	},
+
+	pushQueue: function(){
+		dataRef.push(this.getPushable());
+	}
+
+}
+
+function initFireBase(){
+	writeLog("Initilizing Firebase Object");
+//	dataRef.once('value', updateQueue);
+	dataRef.on('child_added', updateQueue);
+}
+
+function updateQueue(ss){
+	//clear prior queue
+
+	writeLog("Updated Called.");
+	var key = ss.key();
+	var value = ss.val();
+	queue = value;
+	$("#QList").empty();
+	$("#QList").append(queue.getPushable());
+}
+
+function writeLog(d){
+	if(logging)
+		console.log(d);
+}
+
+
+$(function() {
+	initFireBase();
+    queue = new Object(QueueList);
+	//queue.pushQueue();
+});
